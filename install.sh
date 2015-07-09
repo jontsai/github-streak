898053b54c1acf3bfff27a55dfed909efd0eae40
Jonathan Tsai <jontsai@jonathantsai.com>
2015-07-09 15:31:10 +0000

    update streak for 2015-07-09

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@ CRON_DAILY=/etc/cron.daily
 SCRIPT='streakify.sh'
 SRC=${CWD}/${SCRIPT}
 DEST=${CRON_DAILY}/streakify
+TMP=/tmp/streakify
 
 if [[ -d ${CRON_DAILY} ]];
 then
-    echo "Installing symlink to ${DEST}"
-    sudo ln -fs ${SRC} ${DEST}
+    echo "Installing streakify script to ${DEST}"
+    sudo echo -e "#!/bin/bash\ncd ${CWD} && ./${SCRIPT}" > $TMP
+    sudo chmod +x $TMP
+    sudo mv $TMP $DEST
 else
     echo "Missing ${CRON_DAILY} directory. You must install this manually"
 fi
