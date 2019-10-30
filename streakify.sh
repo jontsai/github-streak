392f9dbc1fe7bcdc46543441a56e935365474d6e
Jonathan Tsai <hello@jontsai.com>
2019-10-30 20:31:25 +0000

    allows a specific date for streakify.sh

--- streakify.sh
+++ streakify.sh
@@ -2,10 +2,19 @@
 
 STREAKFILE='.streak'
 
+DATE=$1
+
+if [[ -z $DATE ]]
+then
+    DATE=`date`
+else
+    DATE=`date -d "$DATE 13:33:33"`
+fi
+
 echo 'Keep that streak going... artificially'
 
-echo `date -Iseconds` >> $STREAKFILE
-git commit -a -m "update streak for `date -I`"
+echo `date -Iseconds -d "$DATE"` >> $STREAKFILE
+git commit -a -m "update streak for `date -I -d "$DATE"`" --date="$DATE"
 git push
 
 echo 'Congratulations, you just kept your streak going'
